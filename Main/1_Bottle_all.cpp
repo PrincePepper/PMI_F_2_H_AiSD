09156277c7ad2258203810c42105a0a38f108223
Семен <43268369+PrincePepper@users.noreply.github.com>
2020-04-09 02:16:58 +1000

    1_Bottle_all update - failed

--- Main/1_Bottle_all.cpp
+++ Main/1_Bottle_all.cpp
@@ -3,20 +3,29 @@
 //
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <numeric>
+#include <algorithm>
 
 using namespace std;
 
 //Реализация класса кучи
 class Heap {
-    int *h;        // указатель на массив кучи
+    static const int SIZE = 100000; // максимальный размер кучи
+    int *h;         // указатель на массив кучи
     int HeapSize; // размер кучи
 public:
     explicit Heap(unsigned int SizeHeap);  // конструктор кучи,
-    void addElement(int);  // добавление элемента кучи
-    void out();  // вывод элементов кучи в форме массива
-    int getMax();  // удаление вершины (максимального элемента)
+    void push_heap(int);  // добавление элемента кучи c сортировкой
+    void push_heapNoSort(int);   // добавление элемента кучи
+    void BuildHeap();   // построение кучи по правилу
+    int pop_heap();  // удаление вершины (максимального элемента)
     void heapify(int);  // упорядочение кучи
-    int element_heap(unsigned int element); // доступ к элементам кучи
+    void heapify_old(int);  // упорядочение кучи
+    int element_heap(unsigned int element); // доступ к элементам кучи(изменять нельзя)
+    int *get_heap(unsigned int element); // доступ к элементам кучи(изменять можно)
+    void printOut();  // вывод элементов кучи в форме массива
+    void printOutHeap();  // вывод элементов кучи в форме кучи
 };
 
 //Конструктор кучи
@@ -25,7 +34,14 @@ Heap::Heap(unsigned int SizeHeap) {
     HeapSize = 0;
 }
 
-void Heap::addElement(int n) {
+void Heap::push_heapNoSort(int n) {
+    int i;
+    i = HeapSize;
+    h[i] = n;
+    HeapSize++;
+}
+
+void Heap::push_heap(int n) {
     int i, parent;
     i = HeapSize;
     h[i] = n;
@@ -42,16 +58,31 @@ void Heap::addElement(int n) {
     HeapSize++;
 }
 
-//Вывод элементов кучи в форме массива
-void Heap::out() {
-    for (int i = 0; i < HeapSize; i++) {
-        cout << h[i] << " ";
+void Heap::heapify(int i) {
+    int leftChild, rightChild;
+    int largestChild;
+    while (true) {
+        leftChild = 2 * i + 1;
+        rightChild = 2 * i + 2;
+        largestChild = i;
+        if (leftChild < HeapSize && h[leftChild] > h[largestChild]) {
+            largestChild = leftChild;
+        }
+        if (rightChild < HeapSize && h[rightChild] > h[largestChild]) {
+            largestChild = rightChild;
+        }
+        if (largestChild == i) {
+            break;
+        }
+        int temp = h[i];
+        h[i] = h[largestChild];
+        h[largestChild] = temp;
+        i = largestChild;
     }
-    cout << endl;
 }
 
 //Упорядочение кучи
-void Heap::heapify(int i) {
+void Heap::heapify_old(int i) {
     int left, right;
     int temp;
     left = 2 * i + 1;
@@ -74,18 +105,51 @@ void Heap::heapify(int i) {
     }
 }
 
+void Heap::BuildHeap() {
+    for (int i = HeapSize / 2; i >= 0; i--) {
+        heapify(i);
+    }
+}
+
 //Удаление вершины кучи (максимального элемента)
-int Heap::getMax() {
+int Heap::pop_heap() {
     int x;
     x = h[0];
     h[0] = h[HeapSize - 1];
     HeapSize--;
     heapify(0);
     return (x);
-}
+};
 
 int Heap::element_heap(unsigned int element) {
     return element > HeapSize ? -1 : h[element];
+}
+
+int *Heap::get_heap(unsigned int element) {
+    return &h[element];
+}
+
+
+//Вывод элементов в форме кучи
+void Heap::printOutHeap() {
+    int i = 0;
+    int k = 1;
+    while (i < HeapSize) {
+        while ((i < k) && (i < HeapSize)) {
+            cout << h[i] << " ";
+            i++;
+        }
+        cout << endl;
+        k = k * 2 + 1;
+    }
+}
+
+//Вывод элементов кучи в форме массива
+void Heap::printOut() {
+    for (int i = 0; i < HeapSize; i++) {
+        cout << h[i] << " ";
+    }
+    cout << endl;
 }
 
 int main() {
@@ -98,14 +162,16 @@ int main() {
     for (int i = 0; i < N; i++) {
         int k;
         fin >> k;
-        heap.addElement(k);
+        heap.push_heap(k);
     }
+
     while (M != 0) {
-        int head = heap.getMax();
+        int head = heap.pop_heap();
         head /= 10;
-        heap.addElement(head);
+        heap.push_heap(head);
         M--;
     }
+
     int num = 0;
     for (int i = 0; i < N; i++) {
         num += heap.element_heap(i);
--- test.cpp
+++ test.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    ifstream fin("input.txt");;
+    ofstream fout("output.txt");;
+    if (!fin) return 1;
+
+    int N, M;
+    vector<int> mass;
+
+    fin >> N >> M;
+    if (N < 1 || N > 100000 || M < 1 || M > 100000)
+        return 2;
+
+    int A = 0;
+    for (int i = 0; i < N && fin >> A; i++) {
+        if (A < 0 || A > 1000000000)
+            return 3;
+        mass.push_back(A);
+    }
+
+    for (int i = 0; i < M; i++) {
+//        vector<int>::iterator it = max_element(mass.begin(), mass.end());
+        auto it = max_element(mass.begin(), mass.end());
+        *it /= 10;
+    }
+
+    fout << accumulate(mass.begin(), mass.end(), 0);
 }
