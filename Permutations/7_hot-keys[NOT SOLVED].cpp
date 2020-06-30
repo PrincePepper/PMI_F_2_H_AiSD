7e2dcbbcd22d9246fc78e43eccc3f9217b6f3c9d
PrincePepper <semyon3336@gmail.com>
2020-06-30 19:32:03 +1000

    #Сложные задачи

--- Permutations/7_hot-keys[NOT SOLVED].cpp
+++ Permutations/7_hot-keys[NOT SOLVED].cpp
@@ -1,44 +1,47 @@
 //
-// Created by User on 26.05.2020. НЕ РЕШЕНА
+// Created by User on 26.05.2020.
 //
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
-#include <numeric>
 
 using namespace std;
 
 ifstream fin;
 ofstream fout;
-int ccc = INT_MAX;
-vector<int> ddd;
+int count = 0;
+int t_count = INT_MAX;
+vector<char> bbb;
 
-void bind(vector<bool> &used_char, vector<string> &words, int h, vector<int> &aaa) {
-
+void bind(vector<bool> &used_char, vector<string> &words, int h) {
     if (words.size() == h) {
-        if (ccc > accumulate(aaa.begin(), aaa.end(), 0)) {
-            ddd = aaa;
-            ccc = accumulate(aaa.begin(), aaa.end(), 0);
-        }
+        if(t_count>=count) t_count=count;
         return;
     }
     for (int i = 0; i < words[h].size(); i++) {
         int element = words[h][i];
         if (!used_char[element]) {
             used_char[element] = true;
-            aaa.push_back(i);
-            bind(used_char, words, h + 1, aaa);
-            aaa.pop_back();
+            for (int j = 0; j < i; j++) {
+                count++;
+                bbb.push_back(words[h][j]);
+            }
+            bbb.push_back('&');
+            for (int j = i; j < words[h].size(); j++) {
+                bbb.push_back(words[h][j]);
+            }
+            bind(used_char, words, h + 1);
             used_char[element] = false;
-
         }
         if (i + 1 == words[h].size()) {
-            aaa.push_back(i);
-            bind(used_char, words, h + 1, aaa);
-            aaa.pop_back();
+            for (char &j : words[h]) {
+                bbb.push_back(j);
+            }
+            bind(used_char, words, h + 1);
         }
     }
+    count=0;
 }
 
 int main() {
@@ -55,21 +58,6 @@ int main() {
         fin >> temp;
         words.push_back(temp);
     }
-    vector<int> aaa;
-
-    bind(used_char, words, 0, aaa);
-
-    for(int i =0;i<words.size();i++){
-        int j;
-        for (j = 0; j < ddd[i]; j++) {
-            fout << words[i][j];
-        }
-        fout << "&";
-        for (int h = j; h < words[i].size(); h++) {
-            fout << words[i][h];
-        }
-        fout<<endl;
-    }
-
-    cout << ccc;
+    bind(used_char, words, 0);
+    fout<<t_count;
 }
